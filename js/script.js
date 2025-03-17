d938899c01762b13c8e96ef936bd054537e2dea4
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-03-17 17:16:41 +0000

    added variable js code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Aimar Fernandez
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
 function myButtonClicked() {
-  alert("Hello, World!")
+  // input
+  const streetNumber = document.getElementById("street-number").value
+  const streetName = document.getElementById("street-name").value
+  // output
+  document.getElementById("answer").innerHTML =
+    "Your address is: " + streetNumber + " " + streetName
 }
